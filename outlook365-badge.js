cf41acd7bba6939c89b404ae7f5eb1a5001ca655
Sridhar Setti <sridhar.setti@nike.com>
2019-03-04 14:47:39 -0800

    Fixing outlook script

--- outlook365-badge.js
+++ outlook365-badge.js
@@ -7,10 +7,13 @@ setInterval(updateDockBadge, 15000);
 
 function updateDockBadge() {
   window.fluid.dockBadge = '';
-  var count = document.getElementsByClassName("_1R8zyedlf574PfvRosuwXp _1_HkktuR4sRdjYB0AoBe8f")[0].childNodes[0].textContent;
-  console.log(count);
-  if (count > 0) {
-    window.fluid.dockBadge = count;
+  var unreadNodes = document.getElementsByClassName("_1R8zyedlf574PfvRosuwXp _1_HkktuR4sRdjYB0AoBe8f");
+  if (unreadNodes && unreadNodes.length > 0) {
+    var count = unreadNodes[0].childNodes[0].textContent;
+    console.log(count);
+    if (count > 0) {
+      window.fluid.dockBadge = count;
+    }
   }
 }
 
