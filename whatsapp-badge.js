4d7c45f46b5af82f6d50e857b814818391e25a5b
Sridhar Setti <sridhar.setti@nike.com>
2019-07-22 16:25:26 -0700

    Fixing Whatsapp badge count; Updating Wunderlist dark theme

--- whatsapp-badge.js
+++ whatsapp-badge.js
@@ -7,7 +7,7 @@ setInterval(updateDockBadge, 15000);
 
 function updateDockBadge() {
   window.fluid.dockBadge = '';
-  var items = document.getElementsByClassName("OUeyt");
+  var items = document.getElementsByClassName("P6z4j");
   var itemCount = 0;
   for (var i = 0; i < items.length; i += 1) {
     itemCount += Number(items[i].textContent);
